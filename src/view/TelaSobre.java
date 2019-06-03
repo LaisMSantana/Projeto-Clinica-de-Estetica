0fa480225c20fef351dd5f2d9b9e014f93a39355
Aluno <Aluno@S202M10>
2019-06-03 09:53:19 -0300

    Commit mudanças na tela sobre

--- src/view/TelaSobre.java
+++ src/view/TelaSobre.java
@@ -41,19 +41,19 @@ public class TelaSobre extends JInternalFrame {
 		JTextPane txtpnSobre = new JTextPane();
 		txtpnSobre.setForeground(new Color(0, 0, 0));
 		txtpnSobre.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		txtpnSobre.setText("O Sistema foi desenvolvido com o intuito de automatizar e gerenciar as funcionalidades de uma Cl\u00EDnica Est\u00E9tica, agilizando e armazenando as informa\u00E7\u00F5es dos processos da Cl\u00EDnica. Tem enfoque na atua\u00E7\u00E3o dos Funcion\u00E1rios que seriam os operadores do sistema."
+		txtpnSobre.setText("Bem Vindo você adquiriu o sistema que foi desenvolvido pensando na sua Clínica de Estética, com o intuito de automatizar e gerenciar as funcionalidades de sua Cl\u00EDnica, podendo assim agilizando e armazenando as informa\u00E7\u00F5es dos processos da Cl\u00EDnica. Tem enfoque na atua\u00E7\u00E3o dos Funcion\u00E1rios que seriam os operadores do sistema."
 				+ "\nFoi Desenvolvido por: Jhully, Lais, Natália.");
-		txtpnSobre.setBounds(-6, 284, 622, 135);
+		txtpnSobre.setBounds(10, 239, 622, 135);
 		getContentPane().add(txtpnSobre);
 		
 		JLabel lblImagem = new JLabel("");
-		lblImagem.setIcon(new ImageIcon(TelaSobre.class.getResource("/icones/sobre.png")));
-		lblImagem.setBounds(147, 0, 262, 247);
+		lblImagem.setIcon(new ImageIcon(TelaSobre.class.getResource("/main/java/icones/icons8-sobre-200.png")));
+		lblImagem.setBounds(183, 0, 206, 202);
 		getContentPane().add(lblImagem);
 		
 		JLabel lblVersao = new JLabel("Vers\u00E3o 1.0");
 		lblVersao.setForeground(Color.BLACK);
-		lblVersao.setBounds(237, 239, 116, 15);
+		lblVersao.setBounds(239, 213, 116, 15);
 		getContentPane().add(lblVersao);
 
 	}
