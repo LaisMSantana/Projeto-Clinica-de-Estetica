31e72868c4ec02f0ea7579032e7e1dc08c8d8450
Jhully Muller <jhully.muller06@gmail.com>
2019-06-05 22:16:56 -0300

    alteraçao nome do banco

--- src/dao/Banco.java
+++ src/dao/Banco.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 
 public class Banco {
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String BANCO = "empresa";
+	private static final String BANCO = "estetica";
 	private static final String CONEXAO = "jdbc:mysql://localhost:3306/" + BANCO + "?useTimezone=true&serverTimezone=UTC";
 	private static final String USUARIO = "root";
 	private static final String SENHA = "admin";
