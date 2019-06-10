13c91eae45a5e6158e5f1a1c69f0154b6c85951c
Aluno <Aluno@S202M03>
2019-06-10 10:42:21 -0300

    Commit de Agendamento

--- src/model/dao/AgendamentoDAO.java
+++ src/model/dao/AgendamentoDAO.java
@@ -1,16 +1,44 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import model.vo.Agendamento;
 
 public class AgendamentoDAO {
 
 	public boolean atualizar(Agendamento agendamento) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean sucessoUpdate = false;
+
+		// ****REVISAR QUERY
+		String sql = " UPDATE AGENDAMENTO agendamento SET NOME=?, CPF=?" + " WHERE FUNCIONARIO.ID = ? ";
+
+		Connection conexao = Banco.getConnection();
+		PreparedStatement prepStmt = Banco.getPreparedStatement(conexao);
+
+		try {
+			prepStmt.setString(1, agendamento.getData());
+			//prepStmt.setString(2, agendamento.getCliente());
+		//	prepStmt.setDouble(3, funcionario.getIdFuncionario());
+
+			int codigoRetorno = prepStmt.executeUpdate();
+
+			if (codigoRetorno == 1) {
+				sucessoUpdate = true;
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Erro ao atualizar Funcionário. Causa: " + e.getMessage());
+		} finally {
+			Banco.closePreparedStatement(prepStmt);
+			Banco.closeConnection(conexao);
+		}
+
+		return sucessoUpdate;
 	}
 
 	public int salvar(Agendamento agendamento) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
